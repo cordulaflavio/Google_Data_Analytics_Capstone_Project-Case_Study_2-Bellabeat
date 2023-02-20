7e5a82e05b0dfea739b42c25797644b46dfcd7c0
Flavio Ribeiro Córdula <92122940+cordulaflavio@users.noreply.github.com>
2023-02-20 10:57:17 -0300

    Update cleaning.sql

--- sql/cleaning.sql
+++ sql/cleaning.sql
@@ -40,4 +40,30 @@ SELECT * FROM case_study.heartrate_seconds_merged hsm -- OK
 ---------------- END cleaning heartrate_seconds_merged
 
 
+-- need cleaning / split date and time and add day of week
+SELECT * FROM case_study.hourlycalories_merged hm
+
+----------------  Cleaning hourlycalories_merged
+
+ALTER TABLE case_study.hourlycalories_merged 
+ADD COLUMN activity_date date,
+ADD COLUMN activity_day_name varchar (15),
+ADD COLUMN activity_hour varchar(8);
+
+UPDATE case_study.hourlycalories_merged hm 
+SET activity_date = to_date(hm.activityhour,'mm/dd/yyyy')
+WHERE hm.activityhour IS NOT NULL;
+
+UPDATE case_study.hourlycalories_merged hm
+SET activity_day_name = to_char(activity_date, 'day')
+WHERE hm.activity_date IS NOT NULL;
+
+UPDATE case_study.hourlycalories_merged hm 
+SET activity_hour = to_char(to_timestamp(hm.activityhour,'mm/dd/yyyy hh12:mi:ss AM,PM'),'hh24:mi:ss')
+WHERE hm.activityhour IS NOT NULL;
+
+SELECT * FROM case_study.hourlycalories_merged hm  -- OK 
+
+----------------  END cleaning hourlycalories_merged
+
 
